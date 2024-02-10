f41a0d0fb15d0678f97dc324619f9a0ba8fd6c68
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2024-02-10 14:14:59 +0200

    Connect to mongoDB, add task model, schema and validation, handled errors

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks');
+const connectDB = require('./db/connect');
+require('dotenv').config();
 
 // middleware
 app.use(express.json());
@@ -14,4 +16,13 @@ app.use('/api/v1/tasks', tasks);
 
 const port = 3000;
 
-app.listen(port, console.log(`Server is listening on port ${port}...`));
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server is listening on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,13 +1,20 @@
+const Task = require('../models/task');
+
 const getAllTasks = (req, res) => {
   res.send('Get all tasks');
 };
 
-const createTask = (req, res) => {
-  res.send('Create task');
+const createTask = async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 const getTask = (req, res) => {
-  res.send('Get single task');
+  res.json({ id: req.params.id });
 };
 
 const updateTask = (req, res) => {
