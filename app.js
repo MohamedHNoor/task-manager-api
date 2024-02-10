72770b7116133edb497815d72ceae9acc220527f
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2024-02-10 19:00:52 +0200

    Create async wrapper to refactor the controllers and create custom error handlers

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const tasks = require('./routes/tasks');
 const connectDB = require('./db/connect');
 require('dotenv').config();
 const notFound = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
 
 // middleware
 app.use(express.static('./public'));
@@ -13,8 +14,9 @@ app.use(express.json());
 
 app.use('/api/v1/tasks', tasks);
 app.use(notFound);
+app.use(errorHandlerMiddleware);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
